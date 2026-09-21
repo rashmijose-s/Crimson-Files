428c0d77cfbfeee2b4ac495524a0aecb46c4d55e
Rashmi Jose <rashmijose.s@gmail.com>
2026-09-21 17:54:26 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5002,
+    host: true,
+    allowedHosts: true,
+  },
+})
